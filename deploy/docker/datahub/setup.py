46d9e1e1c4fc09667f81d16340dc67ce573e75e2
Doug Leasure <doug.leasure@protonmail.com>
2022-11-01 10:03:49 +0000

    move api namespace within mysocialwatcher

--- deploy/docker/datahub/setup.py
+++ deploy/docker/datahub/setup.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+
 if __name__ == '__main__':
 
     # environment
--- mysocialwatcher/api/__init__.py
+++ mysocialwatcher/api/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-from api import routes
+from mysocialwatcher.api import routes
 
 # Cross Origin Resource Sharing (for AJAX)
 CORS(app)
