14d31812fc45dcbf55619482bf979c2dfb0a5023
Doug Leasure <doug.leasure@protonmail.com>
2022-11-05 20:53:27 +0000

    Better exception handling in collector/main.py

--- mysocialwatcher/collector/main.py
+++ mysocialwatcher/collector/main.py
@@ -27,6 +27,8 @@ if __name__ == '__main__':
 
     try:
 
+        logger.info('Preparing to collect data.')
+
         # command line argument
         if sys.argv[1] is None:
             files = os.listdir('./specs')
@@ -46,6 +48,16 @@ if __name__ == '__main__':
         os.makedirs(os.path.join(data_dir, 'collecting'), exist_ok=True)
         os.makedirs(os.path.join(data_dir, 'finished'), exist_ok=True)
 
+        # reconfigure temporary file locations
+        constants.DATAFRAME_SKELETON_FILE_NAME = "/skeleton/dataframe_skeleton_" + day_stamp + ".csv"
+        constants.DATAFRAME_TEMPORARY_COLLECTION_FILE_NAME = "/collecting/dataframe_collecting_" + day_stamp + ".csv"
+        constants.DATAFRAME_AFTER_COLLECTION_FILE_NAME = "/finished/dataframe_collected_finished_" + day_stamp + ".csv"
+        constants.DATAFRAME_AFTER_COLLECTION_FILE_NAME_WITHOUT_FULL_RESPONSE = \
+            "/clean/collect_finished_clean" + day_stamp + ".csv"
+
+        temporary_file = data_dir + constants.DATAFRAME_TEMPORARY_COLLECTION_FILE_NAME
+        continue_collection = os.path.exists(temporary_file)
+
         # instantiate watcher
         watcher = watcherAPI(api_version='15.0',
                              sleep_time=12,
@@ -54,34 +66,31 @@ if __name__ == '__main__':
         # load credentials
         watcher.load_credentials_file('credentials.csv')
 
-        # reconfigure temporary file locations
-        constants.DATAFRAME_SKELETON_FILE_NAME = "/skeleton/dataframe_skeleton_" + day_stamp + ".csv"
-        constants.DATAFRAME_TEMPORARY_COLLECTION_FILE_NAME = "/collecting/dataframe_collecting_" + day_stamp + ".csv"
-        constants.DATAFRAME_AFTER_COLLECTION_FILE_NAME = "/finished/dataframe_collected_finished_" + day_stamp + ".csv"
-        constants.DATAFRAME_AFTER_COLLECTION_FILE_NAME_WITHOUT_FULL_RESPONSE = \
-            "/clean/collect_finished_clean" + day_stamp + ".csv"
+    except:
+        logger.error('An error occurred while preparing to collect data.', exc_info=True)
 
-        # collect data
-        temporary_file = data_dir + constants.DATAFRAME_TEMPORARY_COLLECTION_FILE_NAME
-        continue_collection = os.path.exists(temporary_file)
 
-        if continue_collection:
-            logger.info('Continuing: ' + temporary_file)
-            try:
-                df = watcher.load_data_and_continue_collection(
-                    input_file_path=temporary_file)
-            except Exception as e:
-                logger.warning(str(e))
-                continue_collection = False
+    # continue a previous collection
+    if continue_collection:
+        try:
+            logger.info('Continuing a previous collection: ' + temporary_file)
 
-        if not continue_collection:
-            logger.info('Beginning: ' + temporary_file)
+            df = watcher.load_data_and_continue_collection(
+                input_file_path=temporary_file)
+
+        except:
+            logger.warning('An error occurred while continuing a previous collection.', exc_info=True)
+            continue_collection = False
+
+    # start a new collection
+    if not continue_collection:
+        try:
+            logger.info('Beginning a new collection: ' + temporary_file)
+
             df = watcher.run_data_collection(
                 json_input_file_path=specs_filepath,
                 output_dir=data_dir,
                 remove_tmp_files=False)
 
-    except Exception as e:
-
-        # log exception
-        logger.error('Exception:', exc_info=e)
+        except:
+            logger.error('An error occurred while collecting new data.', exc_info=True)
