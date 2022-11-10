0fbc50584950d49d8cbdfb82823c6c6616844b40
Doug Leasure <doug.leasure@protonmail.com>
2022-11-10 15:00:11 +0000

    Create deploy.sh

---
+++ docker/deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+#---- docker ----#
+
+# install docker
+sudo apt-get install ca-certificates curl gnupg lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get update && sudo apt-get install -y docker-compose
+
+# add user to docker group
+sudo usermod -aG docker ubuntu
+
+
+#---- github ----#
+sudo apt install git
+
+# create GitHub deploy key
+cd ~/.ssh
+ssh-keygen
+
+#!! Remember to authorize the key on GitHub
+
+# ssh config for github
+echo "Host github" >> ~/.ssh/config
+echo -e "\tHostName ssh.github.com" >> ~/.ssh/config
+echo -e "\tUser git" >> ~/.ssh/config
+echo -e "\tIdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
+
+# clone repository
+cd ~
+git clone github:OxfordDemSci/mySocialWatcher
+
+# deploy datahub (for example)
+cd ~/mySocialWatcher/docker/datahub
+docker-compose up -d --build
