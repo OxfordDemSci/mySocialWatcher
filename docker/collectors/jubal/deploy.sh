c7c8649375b9719ad88ee6efef7c366c874235a5
Doug Leasure <doug.leasure@protonmail.com>
2022-12-16 21:00:43 +0000

    baseline collection

---
+++ docker/collectors/jubal/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VM="jubal"
+COLLECTIONS=("baseline")
+DIR="/home/ubuntu/mySocialWatcher"
+
+# cron
+sudo cp ${DIR}/docker/collectors/${VM}/${VM}_crontab /etc/cron.d/
+
+# config
+cd ${DIR}
+for COLLECTION in ${COLLECTIONS[@]};
+do
+  python3 ${DIR}/config/collections/${COLLECTION}.py
+done
+
+# docker
+cd ${DIR}/docker/collectors/${VM}
+docker-compose up --build --no-start
+
+unset VM
+unset COLLECTIONS
+unset DIR
