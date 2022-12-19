7f152500d190318414d6143b06faae64c9208dc6
Doug Leasure <doug.leasure@protonmail.com>
2022-12-19 19:10:27 +0000

    Update restore_db.sh

--- docker/datahub/_utils/restore_db.sh
+++ docker/datahub/_utils/restore_db.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # backup directory
-backup_dir=/research/backup/psw_datahub/$(date +"%Y%m%d")
+backup_path=/research/backup/psw_datahub/$(date +"%Y%m%d")/social_media_audience.dump
+
+# # drop database
+# dropdb -h 18.135.72.18 --username=postgres social_media_audience
 
 # remote restore from dump
-pg_restore -h 18.135.72.18 -U postgres -C -d postgres ${backup_dir}/social_media_audience.dump
+pg_restore --host=18.135.72.18 --username=postgres --dbname=postgres --password --clean --create ${backup_path}
 
 # cleanup
-unset backup_date
+unset backup_path
