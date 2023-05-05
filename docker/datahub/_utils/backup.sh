46ceb8321c308952cd432b7226edb6133e212fca
Doug Leasure <doug.leasure@protonmail.com>
2023-05-05 15:54:20 +0100

    increase shm_size for datahub_db container

--- docker/datahub/_utils/backup.sh
+++ docker/datahub/_utils/backup.sh
@@ -21,11 +21,11 @@ pg_dump -h 18.135.72.18 -U postgres -Fc social_media_audience > ${dump_file}
 printf "[`date +'%Y-%m-%d %H:%M:%S'`] Compressing (gzip)\n" >> ${log_file}
 gzip -f ${dump_file}
 
-# delete backups older than 90 days
+# delete backups older than 365 days
 for i in "${past_backups[@]}"
 do
   let x=(`date +%s -d ${current_date}`-`date +%s -d ${i}`)/86400
-  if [ $x -ge 356 ]; then
+  if [ $x -ge 365 ]; then
     printf "[`date +'%Y-%m-%d %H:%M:%S'`] Deleting backup (${x} days old): ${i}\n" >> ${log_file}
     rm -R ${base_dir}/${i}
   fi
