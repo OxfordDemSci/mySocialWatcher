d7d8f2c48ab8b2b148a711ec06934affb368da70
vallerrr <valler4044@gmail.com>
2023-10-11 08:38:53 +0100

    update the function to search for ALL specific category of targeting from FB
    1. update the city/region/country/behaviour targeting record files.
    2. update the code to extract the information
    
    note: token should be added separately in params.py

---
+++ config/specs/specs_explore/organise_return_from_fb.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import json
+import requests
+from config.specs.specs_explore.params import  params_all,url,file_paths
+import string
+from datetime import datetime
+
+
+# params zone
+spec_cat='region'
+json_file_path,csv_file_path= file_paths(spec_cat)
+
+# situation1: directly send request and store the result (return num should be under 1000)
+
+# select the params for specific category, e.g. behaviour, country
+params = params_all[spec_cat]
+
+response = requests.get(url, params=params)
+specs_json = json.loads(response.text)
+content = specs_json['data']
+df = pd.DataFrame(content)
+
+# save jason file
+with open(json_file_path, 'w') as f:
+    f.write(json.dumps(specs_json,indent=4))
+
+# save df
+df.to_csv(csv_file_path, index=False)
+
+
+
+# situation 2: city and region
+def get_city_or_region(params_all,spec_cat,csv_file_path):
+    print(datetime.now())
+    df_city = pd.DataFrame()
+    #df_city = pd.read_csv(csv_file_path)
+    refine_lst = []
+
+    def get_data(q):
+        try:
+            params = params_all[spec_cat]
+            params['q'] = q
+            response = requests.get(url, params=params_all[spec_cat])
+            specs_json = json.loads(response.text)
+            content = specs_json['data']
+            df = pd.DataFrame(content)
+
+            if len(df)==1100:
+                return "another level"
+            else:
+                return df
+        except:
+            print(f'error with {q}, no return')
+            return None
+
+    for q in list(string.ascii_lowercase):
+        print(q)
+        params = params_all[spec_cat]
+
+        re = get_data(q)
+        if isinstance(re, pd.DataFrame):
+            df=re.copy()
+            print(f'for {q},len_df={len(df)}, len df_city = {len(df_city)}')
+
+            df_city = pd.concat([df_city, df], axis=0)
+            df_city.drop_duplicates(inplace=True)
+
+
+        elif isinstance(re, str):
+            for q_2 in list(string.ascii_lowercase):
+                q_combined_2 = q+q_2
+
+
+                re = get_data(q_combined_2)
+                if isinstance(re, pd.DataFrame):
+                    df = re.copy()
+
+                    df_city = pd.concat([df_city, df], axis=0)
+                    df_city.drop_duplicates(inplace=True)
+                    print(f'for {q_combined_2},len_df={len(df)}, len df_city = {len(df_city)}')
+                    df_city.to_csv(csv_file_path)
+                elif isinstance(re, str):
+
+
+                    for q_3 in list(string.ascii_lowercase):
+                        q_combined_3 = q_combined_2 + q_3
+
+                        re = get_data(q_combined_3)
+                        if isinstance(re, pd.DataFrame):
+                            df = re.copy()
+
+                            df_city = pd.concat([df_city, df], axis=0)
+                            df_city.drop_duplicates(inplace=True)
+                            print(f'for {q_combined_3},len_df={len(df)}, len df_city = {len(df_city)}')
+                            df_city.to_csv(csv_file_path)
+                        elif isinstance(re, str):
+                            refine_lst += q_combined_3
+                        else:
+                            continue
+
+                else:
+                    continue
+        else:
+            continue
+
+        df_city.to_csv(csv_file_path)
+    print(datetime.now())
+
+
+get_city_or_region(params_all,spec_cat,csv_file_path)
---
+++ config/specs/specs_explore/params.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+url = "https://graph.facebook.com/v18.0/search"
+token = ''
+params_all = {'behaviour': {'type': 'adTargetingCategory','class': 'behaviors','limit':1000,'access_token': token},
+              'region':{'location_types':['region'],'type':'adgeolocation','q':'','limit':1000,'access_token': token},
+              'country':{'location_types':['country'],'type':'adgeolocation','q':'','limit':1000,'access_token': token},
+              'city':{'location_types':['city'],'type':'adgeolocation','q':'al','limit':1100,'access_token': token}}
+def file_paths(spec_cat):
+    json_file_path = Path.cwd()/f'config/specs/specs_explore/targets_json/{spec_cat}.json'
+    csv_file_path = Path.cwd()/f'config/specs/targets_csv/{spec_cat}.csv'
+    return json_file_path,csv_file_path
