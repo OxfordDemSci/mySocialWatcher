dcdc8e6a63fd1f8a75162d9b71c7681cf9c1c038
Doug Leasure <doug.leasure@protonmail.com>
2023-10-13 15:29:16 +0100

    Update cleanup.sh

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -2,8 +2,8 @@
 data_dir=$1
 
 # delete logs older than 365 days
-find $data_dir -name "*.log" -type f -mtime +365 -exec rm {} \;
-find $data_dir -name "*.log.gz" -type f -mtime +365 -exec rm {} \;
+# find $data_dir -name "*.log" -type f -mtime +365 -exec rm {} \;
+# find $data_dir -name "*.log.gz" -type f -mtime +365 -exec rm {} \;
 
 # compress logs older than 7 days
 find $data_dir -name "*.log" -type f -mtime +7 -exec gzip {} \;
