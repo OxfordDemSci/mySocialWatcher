47eae640cfa1228232e8ec738db45c73db9397e9
caseybreen <caseyfbreen@gmail.com>
2023-12-21 11:31:22 +0000

    create pysocialwatcher_opt_flag to boolean

--- mysocialwatcher/collector/main.py
+++ mysocialwatcher/collector/main.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
     logger.info('Sleep time: ' + sleep_time)
     logger.info('pysocialwatcher_opt: ' + pysocialwatcher_opt_flag)
 
+    # set pysocialwatcher_opt_flag to boolean 
+    pysocialwatcher_opt_flag = (pysocialwatcher_opt_flag.lower() == 'true')
 
     specs_list = get_specs_list(specs_dir=specs_dir,
                                 data_dir=data_dir)
