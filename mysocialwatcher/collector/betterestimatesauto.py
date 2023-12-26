f89953683f2cea3d20b2b59e143edcb5e2541df5
caseybreen <caseyfbreen@gmail.com>
2023-12-26 16:27:12 +0000

    Update countries to try to exclude african countries

--- mysocialwatcher/collector/betterestimatesauto.py
+++ mysocialwatcher/collector/betterestimatesauto.py
@@ -25,20 +25,25 @@ time_wait = 0 # waits 5 minutes between tries
 
 #countries_to_try = ["US","CA"]
 
-countries_to_try = ["AD","AE","AF","AG","AI","AL","AM","AO","AR","AS","AT","AU","AW","AZ","BA","BB",
-                    "BD","BE","BF","BG","BH","BI","BJ","BL","BM","BN","BO","BQ","BR","BS","BT","BW","BY","BZ",
-                    "CA","CD","CF","CG","CH","CI","CK","CL","CM","CN","CO","CR","CV","CW","CY","CZ","DE",
-                    "DJ","DK","DM","DO","DZ","EC","EE","EG","ER","ES","ET","FI","FJ","FK","FM","FO","FR","GA",
-                    "GB","GD","GE","GF","GG","GH","GI","GL","GM","GN","GP","GQ","GR","GT","GU","GW","GY","HK",
-                    "HN","HR","HT","HU","ID","IE","IL","IM","IN","IQ","IS","IT","JE","JM","JO","JP","KE",
-                    "KG","KH","KI","KM","KN","KR","KW","KY","KZ","LA","LB","LC","LI","LK","LR","LS","LT","LU","LV",
-                    "LY","MA","MC","MD","ME","MF","MG","MH","MK","ML","MM","MN","MO","MP","MQ","MR","MS","MT","MU",
-                    "MV","MW","MX","MY","MZ","NA","NC","NE","NG","NI","NL","NO","NP","NR","NZ","OM","PA",
-                    "PE","PF","PG","PH","PK","PL","PM","PR","PS","PT","PW","PY","QA","RE","RO","RS","RW",
-                    "SA","SB","SC","SE","SG","SH","SI","SJ","SK","SL","SM","SN","SO","SR","SS","ST","SV","SX","SZ",
-                    "TC","TD","TG","TH","TJ","TL","TM","TN","TO","TR","TT","TV","TW","TZ",
-                    "UA","UG","US","UY","UZ","VC","VE","VG","VI","VN","VU","WF","WS","XK",
-                    "YE","YT","ZA","ZM","ZW"]
+countries_to_try = ["AD", "AE", "AF", "AG", "AI", "AL", "AM", "AR", "AS", "AT",
+                    "AU", "AW", "AZ", "BA", "BB", "BD", "BE", "BG", "BH", "BL",
+                    "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BY", "BZ", "CA",
+                    "CH", "CK", "CL", "CN", "CO", "CR", "CW", "CY", "CZ", "DE",
+                    "DK", "DM", "DO", "EC", "EE", "ES", "FI", "FJ", "FK", "FM",
+                    "FO", "FR", "GB", "GD", "GE", "GF", "GG", "GI", "GL", "GP",
+                    "GR", "GT", "GU", "GY", "HK", "HN", "HR", "HT", "HU", "ID",
+                    "IE", "IL", "IM", "IN", "IQ", "IS", "IT", "JE", "JM", "JO",
+                    "JP", "KG", "KH", "KI", "KN", "KR", "KW", "KY", "KZ", "LA",
+                    "LB", "LC", "LI", "LK", "LT", "LU", "LV", "MC", "MD", "ME",
+                    "MF", "MH", "MK", "MM", "MN", "MO", "MP", "MQ", "MS", "MT",
+                    "MV", "MX", "MY", "NC", "NI", "NL", "NO", "NP", "NR", "NZ",
+                    "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PR",
+                    "PS", "PT", "PW", "PY", "QA", "RE", "RO", "RS", "SA", "SB",
+                    "SE", "SG", "SH", "SI", "SJ", "SK", "SM", "SR", "SV", "SX",
+                    "TC", "TH", "TJ", "TL", "TM", "TO", "TR", "TT", "TV", "TW",
+                    "UA", "US", "UY", "UZ", "VC", "VE", "VG", "VI", "VN", "VU",
+                    "WF", "WS", "XK", "YE", "YT"]
+
 
 ##
 constants.SLEEP_TIME = 0
