69da892035bc62aba7f53ffc4df9695c5c468552
caseybreen <caseyfbreen@gmail.com>
2024-01-03 19:26:26 +0000

    add dgg_subnational_opt

--- docker/collectors/badger/deploy.sh
+++ docker/collectors/badger/deploy.sh
@@ -2,7 +2,7 @@
 
 
 VM="badger"
-COLLECTIONS=("dgg_national" "dgg_national_ins" "dgg_national_home_recent" "dgg_subnational_age" "dgg_subnational_education" "dgg_subnational_device")
+COLLECTIONS=("dgg_national" "dgg_national_ins" "dgg_national_home_recent" "dgg_national_opt" "dgg_subnational_age" "dgg_subnational_education" "dgg_subnational_device")
 DIR="/home/ubuntu/mySocialWatcher"
 
 # cron
