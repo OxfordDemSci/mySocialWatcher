d0a2bdb1e10ab21d12949f7c451197abfe92cb77
caseybreen <caseyfbreen@gmail.com>
2024-01-07 19:48:21 +0000

    add age X education and age X device collections

---
+++ config/collections/dgg_subnational_age_education.py
@@ -0,0 +1,49 @@
+import os
+import json
+import shutil
+import pandas as pd
+
+# virtual machine and collection names
+vm = 'badger'
+collection = 'dgg_subnational_age'
+
+
+if __name__ == '__main__':
+
+    # ---- paths ---- #
+    master_credentials_path = os.path.join('config', 'private', 'credentials_master.csv')
+    specs_template_path = os.path.join('config', 'specs', 'examples', 'dgg_subnational_age_education')
+    out_dir = os.path.join('docker', 'collectors', vm, collection)
+    os.makedirs(out_dir, exist_ok=True)
+
+    # ---- credentials ---- #
+
+    # path for output credentials.csv
+    credentials_path = os.path.join(out_dir, 'credentials.csv')
+
+    # load master credentials
+    master_credentials = pd.read_csv(master_credentials_path)
+
+    # filter vm and collection
+    credentials = master_credentials.loc[(master_credentials.vm == vm) &
+                                         (master_credentials.collection == collection)]
+
+    # save to csv
+    credentials.to_csv(credentials_path,
+                       columns=['token', 'app'],
+                       header=False,
+                       index=False)
+
+    # ---- collection specs ---- #
+
+    # output directory
+    specs_dir = os.path.join(out_dir, 'specs')
+    os.makedirs(specs_dir, exist_ok=True)
+
+    # cleanup old specs
+    for f in os.listdir(specs_dir):
+        os.remove(os.path.join(specs_dir, f))
+
+    specs = [file for file in os.listdir(specs_template_path) if file.endswith('.json')]
+    for spec in specs:
+        shutil.copy(os.path.join(specs_template_path, spec), os.path.join(specs_dir, spec))
