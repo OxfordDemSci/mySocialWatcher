3fb7808b33452d7b8fdfc8b6695ab4b1d1659b0a
edarin <edith.darin@demography.ox.ac.uk>
2024-04-03 09:20:58 +0100

    remove comment

--- mysocialwatcher/crawler/main.py
+++ mysocialwatcher/crawler/main.py
@@ -2,7 +2,7 @@ from mysocialwatcher.crawler.utils import *
 from dotenv import load_dotenv
 
 load_dotenv()  # load_dotenv('./config/private/crawler.env')
-token = os.environ.get('API_TOKEN')  # token = 'ee0b578690a08e329038fabb567294b5' / API token = '50e62dc0a02dab6ddd6afb100dce3eee'
+token = os.environ.get('API_TOKEN')  # 
 url = os.environ.get('API_URL')  # url = 'http://127.0.0.1/api/v1/write' / ASW API url = 'http://18.135.72.18/api/v1/social_media_audience/write'
 data_dir = os.environ.get('DATA_DIR')  # data_dir = 'C:/Users/edithd/Documents/mySocialWatcher/data/saffron'
 
