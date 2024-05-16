2a49c1f0df45f6b802161cbe4fb7407ca8c94e9b
Doug Leasure <doug.leasure@protonmail.com>
2024-05-16 13:20:22 +0100

    update collector deploy.sh scripts

--- docker/collectors/monty/deploy.sh
+++ docker/collectors/monty/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VM="monty"
-COLLECTIONS=("ukraine_admin2_tessellation")
+COLLECTIONS=("ukraine_admin2_tessellation_homerecent")
 DIR="/home/ubuntu/mySocialWatcher"
 
 # cron
@@ -16,7 +16,7 @@ done
 
 # docker
 cd ${DIR}/docker/collectors/${VM}
-docker-compose up --build --no-start
+docker compose up --build --no-start
 
 unset VM
 unset COLLECTIONS
