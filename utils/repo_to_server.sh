3d4db66e51a91a80f87817ccf04d5bce4b53cbbe
doug-leasure <douglas.leasure@demography.ox.ac.uk>
2025-07-09 18:15:58 +0100

    revise nginx config

--- utils/repo_to_server.sh
+++ utils/repo_to_server.sh
@@ -2,7 +2,7 @@
 
 # command line arguments
 local_folder=$1
-remote_folder=$1
+remote_folder=$2
 
 # rsync folder
 rsync -rvhe ssh --exclude-from='./utils/repo_to_server_exclude.txt' $local_folder $remote_folder
